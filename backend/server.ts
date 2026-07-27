f1f5d9f58364b2f224ef9ec6476cb0f03e22c550
amir-haghighi <hagh73h@gmail.com>
2026-07-27 02:45:07 -0700

    server port changed + minor changes in server

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,12 @@
-import express from "express"
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+
+import express from "express";
 import userRouter from "./routes/userRouter.js";
 import mongoose from "mongoose";
-import configDotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRouter.js";
-import cors from "cors"
+import cors from "cors";
 import productRouter from "./routes/productRouter.js";
 import orderRouter from "./routes/orderRouter.js";
 import path from "path";
@@ -22,14 +24,13 @@ app.use(cors({
     origin: true,
     credentials: true
 }));
-app.set("query parser", "extended")
+app.set("query parser", "extended");
 
-configDotenv.config({ path: "./config.env" })
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/auth", authRouter)
-app.use("/api/v1/products", productRouter)
-app.use("/api/v1/orders", orderRouter)
-app.use("/api/v1/cart", cartRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/orders", orderRouter);
+app.use("/api/v1/cart", cartRouter);
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -41,7 +42,9 @@ mongoose
     .connect(MONGODB_URI)
     .then(() => console.log("db connected successfully!"))
     .catch((err) => console.error("db connection error:", err));
-const PORT = 4000
-app.listen(PORT, "0.0.0.0", () => {
-    console.log("server is running on the port: ", PORT)
-})
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
+
+app.listen(PORT, () => {
+    console.log("server is running on the port:", PORT);
+});
